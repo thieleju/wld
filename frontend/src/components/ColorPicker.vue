a138302f9e5dfaf847d4b86221e1d88f46d2d7f8
Julian 41455100+thieleju@users.noreply.github.com <thiele.juli@gmail.com>
2023-06-13 02:14:24 +0200

    Added basic component

---
+++ frontend/src/components/ColorPicker.vue
@@ -0,0 +1,30 @@
+<template>
+  <!-- color picker -->
+  <div class="d-flex justify-space-around">
+    <v-card class="d-flex flex-column">
+      <v-color-picker
+        v-model="color_current"
+        v-model:mode="mode"
+        :modes="modes"
+      ></v-color-picker>
+      <v-btn block color="primary" @click="$emit('save', color_current)"
+        >Save</v-btn
+      >
+    </v-card>
+  </div>
+</template>
+
+<script setup>
+import { ref } from "vue";
+
+const props = defineProps({
+  color: {
+    type: String,
+    default: "#000000",
+  },
+});
+
+const color_current = ref(props.color);
+const mode = ref("hexa");
+const modes = ref(["hsla", "rgba", "hexa"]);
+</script>
