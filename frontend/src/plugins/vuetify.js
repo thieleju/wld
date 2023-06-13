9d55f667af1bd68e5c65a2ed1185de5a1d8a5dc8
Julian <thiele.juli@gmail.com>
2023-06-13 02:14:24 +0200

    Added basic component

--- frontend/src/plugins/vuetify.js
+++ frontend/src/plugins/vuetify.js
@@ -5,22 +5,29 @@
  */
 
 // Styles
-import '@mdi/font/css/materialdesignicons.css'
-import 'vuetify/styles'
+import "@mdi/font/css/materialdesignicons.css";
+import "vuetify/styles";
 
 // Composables
-import { createVuetify } from 'vuetify'
+import { createVuetify } from "vuetify";
 
 // https://vuetifyjs.com/en/introduction/why-vuetify/#feature-guides
 export default createVuetify({
   theme: {
+    defaultTheme: "dark",
     themes: {
       light: {
         colors: {
-          primary: '#1867C0',
-          secondary: '#5CBBF6',
+          primary: "#03F5A0",
+          secondary: "#10DAC5",
+        },
+      },
+      dark: {
+        colors: {
+          primary: "#03F5A0",
+          secondary: "#10DAC5",
         },
       },
     },
   },
-})
+});
--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,26 +1,42 @@
 // Composables
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHistory } from "vue-router";
 
 const routes = [
   {
-    path: '/',
-    component: () => import('@/layouts/default/Default.vue'),
+    path: "/",
+    component: () => import("@/layouts/default/Default.vue"),
     children: [
       {
-        path: '',
-        name: 'Home',
+        path: "",
+        name: "Home",
         // route level code-splitting
         // this generates a separate chunk (about.[hash].js) for this route
         // which is lazy-loaded when the route is visited.
-        component: () => import(/* webpackChunkName: "home" */ '@/views/Home.vue'),
+        component: () =>
+          import(/* webpackChunkName: "home" */ "@/views/Home.vue"),
+        meta: {
+          title: "0/0/0 - WLD",
+        },
       },
     ],
   },
-]
+  // fallback route
+  {
+    path: "/:pathMatch(.*)*",
+    redirect: "/",
+  },
+];
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
   routes,
-})
+});
 
-export default router
+// add navigation guards here
+router.beforeEach((to, from, next) => {
+  // set page title
+  document.title = to.meta.title;
+  next();
+});
+
+export default router;
